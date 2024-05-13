534d8f7cd937b0eb44a658f9b6caed99a03458fb
Jonas Sjödin <jonas.k.sjodin@gmail.com>
2024-05-13 22:56:39 +0200

    Fixing invalid reference with plugin script for EST

--- kustomize-plugins/external-secrets-transformer.sh
+++ kustomize-plugins/external-secrets-transformer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 resourceList=$(cat) # read the `kind: ResourceList` from stdin
 
-storeKind=$(echo "$resourceList" | sed -n -e 's/storeKind: //p')
-storeName=$(echo "$resourceList" | sed -n -e 's/storeName: //p')
+storeKind=$(echo "$resourceList" | sed -n -e 's/storeKind: //p' | xargs)
+storeName=$(echo "$resourceList" | sed -n -e 's/storeName: //p' | xargs)
 
 export STORE_NAME="$storeName"
 export STORE_KIND="$storeKind"
