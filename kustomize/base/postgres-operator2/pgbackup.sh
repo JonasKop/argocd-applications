ee17e615bf154d85e3638943e08149fd4be2ca3a
Jonas Sjödin <jonas.k.sjodin@gmail.com>
2024-07-21 00:02:00 +0200

    deleting

--- kustomize/base/postgres-operator2/pgbackup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-NAME=home-assistant
-NAMESPACE=home-assistant
-
-pgUsername=$(kubectl get secret -n "$NAMESPACE" "$NAME"."$NAME"-postgresql.credentials.postgresql.acid.zalan.do -o yaml | yq '.data.username' | base64 -d)
-pgPassword=$(kubectl get secret -n "$NAMESPACE" "$NAME"."$NAME"-postgresql.credentials.postgresql.acid.zalan.do -o yaml | yq '.data.password' | base64 -d)
-
-PGPASSWORD=$pgPassword pg_dumpall -U "$pgUsername" -h localhost > pgdump
